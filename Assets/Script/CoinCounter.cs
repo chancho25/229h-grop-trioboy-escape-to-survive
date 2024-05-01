2a3ff35c95dddb543aa46539a1f90619d90283cf
chanchon <chanchol.wong@bumail.net>
2024-05-01 16:02:02 +0700

    change position

---
+++ Assets/Script/CoinCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class CoinCounter : MonoBehaviour
+{
+    public static CoinCounter instance;
+    public TMP_Text coinText;
+    public int currenCoint = 0;
+
+    void Awake()
+    {
+
+        instance = this;
+
+    }
+    void Start()
+    {
+        coinText.text = "COINS: " + currenCoint.ToString();
+    }
+
+    public void IncreaseCoins(int v)
+    {
+
+        currenCoint += v;
+        coinText.text = "COINS: " + currenCoint.ToString();
+
+    }
+}
